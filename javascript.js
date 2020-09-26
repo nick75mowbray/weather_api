125f6a04deb3ad9c3172e0f53411e1a9b449c381
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-26 16:49:38 +0930

    api now interacts with page and adds forcasts using js rather than hmtl

--- javascript.js
+++ javascript.js
@@ -15,13 +15,28 @@ $(document).ready(function(){
             console.log(icon);
             lat = response.coord.lat;
             long = response.coord.lon;
-            $('#test').append("<img src='http://openweathermap.org/img/wn/"+icon+"@2x.png'>");
+            // add elements to page
+            $('#today-icon').append("<img src='http://openweathermap.org/img/wn/"+icon+"@2x.png'>");
+            $('#today-temp').text(Math.floor(parseInt(response.main.temp)));
+            $('#today-weather').text(response.weather[0].main);
+            $('#humidity').text(response.main.humidity+"%");
+            $('#windspeed').text(response.wind.speed+" m/s");
             var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+long+"&appid="+APIkey;
       $.ajax({
         url: uvURL,
         method: "GET",
         success: function(response){
-            console.log(response);
+            $('#uv-rating').text(response.value);
+            if (response.value < 4){
+                $('#uv-text').text("low");
+                $('#uv-container').css('background-color', 'blue');
+            } else if (response.value > 3 && response.value < 10){
+                $('#uv-text').text("moderate");
+                $('#uv-container').css('background-color', 'green');
+            } else {
+                $('#uv-text').text("high");
+                $('#uv-container').css('background-color', 'red');
+            }
         },
         error: function(){
             console.log("oops something went wrong");
@@ -38,12 +53,35 @@ $(document).ready(function(){
           console.log("toggle search");
       }
       $('#arrow').on("click", toggleSearch);
+      $('#city-name').on("click", toggleSearch);
       $('#close-div').on("click", toggleSearch);
       
-// get stuff:
-// humidity response.main.humidity
-// temperature response.main.temp
-// weather type response.weather.main
-// wind speed response.wind.speed (in m/s)
+//   5 day forcast
+var forcastURL = "http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+APIkey+"&units=metric";
+$.ajax({
+    url: forcastURL,
+    method: "GET",
+    success: function(response){
+        for (var i = 0; i < 5; i++){
+            var card = $("<div class='card'></div>");
+            var day = $("<h4 class='day'></h4>").text(moment().add((i+1), 'days').format('ddd'));
+            // var tempContainer = "<div class='temp-container></div";
+            var temp = $("<h2 class='forcast-temp'></h2>").text(Math.floor(parseInt(response.list[i].main.temp))).append("&#176;");
+            var forcastIcon = response.list[i].weather[0].icon;
+            var forcastIMG = $("<img src='http://openweathermap.org/img/wn/"+forcastIcon+".png'>");
+            card.append(day, temp, forcastIMG);
+            $('#forcast-container').append(card);
+    }
+},
+    error: function(){
+        console.log("oops something went wrong");
+    }
+})
 
+// display correct days for forcast
+$('#day-1').text((moment().add(1, 'days').format('ddd')));
+$('#day-2').text((moment().add(2, 'days').format('ddd')));
+$('#day-3').text((moment().add(3, 'days').format('ddd')));
+$('#day-4').text((moment().add(4, 'days').format('ddd')));
+$('#day-5').text((moment().add(5, 'days').format('ddd')));
 });//end document ready
